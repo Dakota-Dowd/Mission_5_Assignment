2a868be1b455b0191ab1d4bea2537bf02c5adaf4
Dakota <Jmdowd2020@gmail.com>
2026-02-04 15:11:38 -0700

    initial commit

---
+++ Mission_5_Assignment/wwwroot/js/calculator.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#calculateButton").click(function () {
+        // Get the values from the inputs
+        var hours = $("#hours").val();
+        var rate = $("#rate").val();
+
+        // Validate the inputs
+        if (hours <= 0 || isNaN(hours) || hours === "") {
+            alert("Please enter a valid positive number for hours");
+            retun;
+        }
+
+        // Calculate total
+        var total = hours * rate;
+
+        $("#totalOutput").val("$" + total.toFixed(2));
+    })
+})
